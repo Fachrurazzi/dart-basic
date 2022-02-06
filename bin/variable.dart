e5a82c218a8c09baa95c14093227bf4876bf0a89
Fachrurazzi <razzytirta@gmail.com>
2022-02-07 07:17:34 +0800

    Variable

---
+++ bin/variable.dart
@@ -0,0 +1,33 @@
+void main() {
+  // St ring name;
+  // name = "Fachrurazzi";
+
+  // String name = "Fachrurazzi";
+
+  var name = "Fachrurazzi";
+  name = "Joko";
+
+  print(name);
+
+  final firstName = "Razzy";
+  print(firstName);
+
+  // perbedaan kata kunci final dan const
+  final array1 = [1, 2, 3];
+  const array2 = [1, 2, 3];
+
+  array1[0] = 10;
+  // array2[0] = 10;
+
+  print(array1);
+  print(array2);
+
+  late var value = getValue();
+  print("Variable sudah dibuat");
+  print(value);
+}
+
+String getValue() {
+  print('getValue dipanggil');
+  return 'Fachrurazzi';
+}
