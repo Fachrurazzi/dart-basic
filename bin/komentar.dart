8b6c6f7e569f486d10ba23ce88c882e23ce9e717
Fachrurazzi <razzytirta@gmail.com>
2022-02-07 07:21:55 +0800

    Komentar

---
+++ bin/komentar.dart
@@ -0,0 +1,13 @@
+/// this is main function
+/// will executed by Dart
+void main() {
+
+  // this is name variable
+  var name = "Fachrurazzi";
+
+    /*
+    this is multi line comment
+    you can add more here
+    */
+  print(name);
+}
