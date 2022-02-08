ba7c0ea942dfc398f46a96d9b872c3d71bd0b94a
Fachrurazzi <razzytirta@gmail.com>
2022-02-08 10:12:59 +0800

    Operator Perbandingan

---
+++ bin/operator_perbandingan.dart
@@ -0,0 +1,14 @@
+void main() {
+
+
+  print(10 == 10);
+  print(10 != 10);
+  print(10 > 10);
+  print(10 < 10);
+  print(10 >= 10);
+  print(10 <= 10);
+
+  print('razzy' == 'razzy');
+  print('razzy' != 'razzy');
+
+}
