5d7d628dcdfbe4f07183c83512953636e7d48952
Fachrurazzi <razzytirta@gmail.com>
2022-02-10 15:57:31 +0800

    Function Parameter

---
+++ bin/function_parameter.dart
@@ -0,0 +1,9 @@
+
+void sayHello(String firstName, String lastName) {
+  print('Hai $firstName $lastName');
+}
+void main() {
+  var firstName = 'Razzi';
+  var lastName = 'Tirta';
+  sayHello(firstName, lastName);
+}
